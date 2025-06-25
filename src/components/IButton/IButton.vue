030ac6e77337a5e30d71d2e4033975d229313559
Oleh Shalashov <spirit14081992@gmail.com>
2025-06-25 08:49:56 -0300

    Add first page with styles

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,7 +1,6 @@
 <template>
-  <button class="btn bg-[#ffa270] rounded-xl py-[14px] px-10 text-white font-bold -tracking-wider">
+  <button class="btn bg-[#ffa270] rounded-xl py-[14px] px-10 text-white font-bold -tracking-wider hover:bg-[#ffb58d] duration-300 ease-in-out">
     Почати роботу
   </button>
 </template>
 
-<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 export default {
   content: ['./src/**/*.{js,ts,vue,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F3743D',
+      },
+    },
   },
   plugins: [],
 }
 
+//* In extend section can be added global variables, such as color sets.
