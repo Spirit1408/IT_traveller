2c06763ff61e64028d72fcc62fe8b783cc139b05
Oleh Shalashov <spirit14081992@gmail.com>
2025-06-26 11:52:17 -0300

    Add example of usage of named slots

--- src/components/FavoritePlaces/FavoriePlaces.vue
+++ src/components/FavoritePlaces/FavoriePlaces.vue
@@ -1,15 +1,17 @@
 <script setup>
-import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
-import IButton from '../IButton/IButton.vue';
+import IButton from '../IButton/IButton.vue'
 </script>
 
 <template>
   <div class="px-6">
     <div class="text-gray mb-4">Додані маркери</div>
 
-    <FavoritePlace v-for="n in 4" :key="n" />
+    <slot name="list"></slot>
 
     <IButton class="mt-10 w-full">Додати маркер</IButton>
   </div>
 </template>
-// Using v-for directive to render FavoritePlace component 4 times.
+// Using v-for directive to render FavoritePlace component 4 times. Also can use v-slot (or #) to
+create named slots in components. Tag 'slot' will have attribute 'name', to which we will refer
+while adding extra elements to the component. Adding extra elements to the component will be
+possible by using template with v-slot (or just #{name}) directive.
