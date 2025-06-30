ab053b78ca3efadcc3f9a05df575d46709d35f4b
Oleh Shalashov <spirit14081992@gmail.com>
2025-06-30 10:01:54 -0300

    Add IModal component with opening/closing logic

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@ import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 import FavoritePlace from './components/FavoritePlace/FavoritePlace.vue'
 import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
 import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
+import IModal from './components/IModal/IModal.vue'
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+const closeModal = () => (isOpen.value = false)
+const openModal = () => (isOpen.value = true)
 </script>
 
 <template>
@@ -15,4 +21,9 @@ import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
 
   <RegistrationForm @submit="console.log" />
   <LoginForm @submit="console.log" />
+  <IModal @close="closeModal" v-if="isOpen">Hello modal!</IModal>
+
+  <button @click="openModal">Open modal</button>
 </template>
+
+// IModal uses emit "close" to close the modal by clicking on the cross icon. IModal component has condition to render only when isOpen is true and executing emitted close function to change isOpen state. Currently IModal component is rendered in App component, but it can be rendered in any component (on a body level). It can be implemented by using Teleport component (wrapping the IModal component in it).
--- src/components/Auth/LoginForm/LoginForm_watch.vue
+++ src/components/Auth/LoginForm/LoginForm_watch.vue
@@ -2,7 +2,7 @@
 import FormContainer from '../FormContainer.vue'
 import IButton from '@/components/IButton/IButton.vue'
 import IInput from '@/components/IInput/IInput.vue'
-import { reactive, toRaw } from 'vue'
+import { reactive, toRaw, watch } from 'vue'
 
 const emit = defineEmits(['submit'])
 
@@ -10,6 +10,14 @@ const userData = reactive({
   email: '',
   password: '',
 })
+
+watch(
+  userData,
+  () => {
+    console.log('User data changed!', userData)
+  },
+  { immediate: true },
+)
 </script>
 
 <template>
@@ -26,3 +34,6 @@ const userData = reactive({
     <IButton class="mt-10 w-full" variant="gradient" type="submit">Увійти</IButton>
   </FormContainer>
 </template>
+
+// watch - functionality for monitoring changes of selected data. Extra parameter "immediate" - for
+immediate execution of the function (when component is mounted).
