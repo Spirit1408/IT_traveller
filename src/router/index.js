67d2dc11f9e95e566f6dda97986237f88596dfdf
Oleh Shalashov <spirit14081992@gmail.com>
2025-07-03 11:55:49 -0300

    Add lazy loading feature for router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import GreetingView from '@/views/GreetingView.vue'
-import AuthView from '@/views/AuthView.vue'
-import HomepageView from '@/views/HomepageView.vue'
+
+const GreetingView = () => import('../views/GreetingView.vue')
+const AuthView = () => import('../views/AuthView.vue')
+const HomepageView = () => import('../views/HomepageView.vue')
 
 const routes = [
   { path: '/', component: GreetingView },
@@ -15,3 +16,7 @@ export const router = createRouter({
 })
 
 // File for organizing routes. Array routes contains objects, each of them describes path of the route and contains component (note that we are importing components of the pages here and not in the App component), which will be rendered on this path. Router is an instance, which requires created history of routing (from where to where we are going by pages) and array of routes itself. Connecting this router in main.js file to our app. In App component using RouterView to get access to the pages.
+
+// By default, all the components will be imported to here at once. For better optimization we can use lazy loading - importing components only when they will be needed using function, wich will return import() function for the component. In this way the component of the page will be imported only when it will be needed, because when entering on the page - route of this page will call not an imported component itself, but import of the component.
+
+// history will help for navigation purposes (using arrows back and forward in the browser to go back and forward between visited pages).
