872527c379a1e4d80bdee117868b43e19a065395
Oleh Shalashov <spirit14081992@gmail.com>
2025-07-04 09:56:11 -0300

    Add basic endpoints

---
+++ src/api/clientFetch.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const clientFetch = axios.create({
+  baseURL: "https://back-for-vue.b.goit.study/"
+})
+// Setting up axios instance for working with API requests.
---
+++ src/api/favorite-places/index.js
@@ -0,0 +1,21 @@
+import { clientFetch } from '../clientFetch'
+
+const BASE_PLACES_URL = '/points'
+
+export const getFavoritePlaces = () => {
+  return clientFetch.get(BASE_PLACES_URL)
+}
+
+export const addFavoritePlace = (body) => {
+  return clientFetch.post(BASE_PLACES_URL, body)
+}
+
+export const updateFavoritePlace = (body) => {
+  return clientFetch.put(BASE_PLACES_URL, body)
+}
+
+export const deleteFavoritePlace = (id) => {
+  return clientFetch.delete(BASE_PLACES_URL, id)
+}
+
+// API for working with favorite places
