23de2702341e85b1feacc9e073bcbdf6521f2d48
Oleh Shalashov <spirit14081992@gmail.com>
2025-07-08 11:00:30 -0300

    Implement feautre of adding marker on the map

--- src/views/HomepageView_static_markers.vue
+++ src/views/HomepageView_static_markers.vue
@@ -12,7 +12,6 @@ const { style, apiToken } = mapSettings
 const favoritePlaces = ref([])
 const activeId = ref(null)
 const map = ref(null)
-const mapMarkerLngLat = ref(null)
 
 const changeActiveId = (id) => {
   activeId.value = id
@@ -22,18 +21,6 @@ const changePlace = (id) => {
   const { lngLat } = favoritePlaces.value.find((place) => place.id === id)
   changeActiveId(id)
   map.value.flyTo({ center: lngLat })
-}
-
-const handleMapClick = ({ lngLat }) => {
-  if (
-    mapMarkerLngLat.value &&
-    mapMarkerLngLat.value[0] === lngLat.lng &&
-    mapMarkerLngLat.value[1] === lngLat.lat
-  ) {
-    return (mapMarkerLngLat.value = null)
-  }
-
-  mapMarkerLngLat.value = [lngLat.lng, lngLat.lat]
 }
 
 onMounted(async () => {
@@ -56,18 +43,8 @@ onMounted(async () => {
         :center="[-38.94825316249595, -15.683918784057834]"
         :zoom="13"
         @mb-created="(mapInstance) => (map = mapInstance)"
-        @mb-click="handleMapClick"
       >
-        <MapboxMarker v-if="mapMarkerLngLat" :lngLat="mapMarkerLngLat" anchor="bottom">
-          <MarkerIcon class="h-8 w-8" />
-        </MapboxMarker>
-
-        <MapboxMarker
-          v-for="place in favoritePlaces"
-          :key="place.id"
-          :lngLat="place.lngLat"
-          anchor="bottom"
-        >
+        <MapboxMarker v-for="place in favoritePlaces" :key="place.id" :lngLat="place.lngLat">
           <button @click="changeActiveId(place.id)">
             <MarkerIcon class="h-8 w-8" :isActive="place.id !== activeId" />
           </button>
@@ -77,12 +54,6 @@ onMounted(async () => {
   </main>
 </template>
 
-// Implementing feature of addin a marker on the map. Adding custom marker in Mapbox, which will be
-shown just if marker is setted (has its coordinates). Adding event mb-click to get coordinates of
-the marker while clicking somewhere on the map, through the handler. While clicking on the map -
-coordinates from the click event will be stored in "mapMarkerLngLat" variable. What will cause
-showing our custom marker element on the map. Why using handler? Because lngLat stored in object in
-event, we need to get the coordinates and put them to the array, which will be stored in
-"mapMarkerLngLat", value of which (array) will be passed to MapboxMarker prop lngLat. Clicking on
-the marker with the same coordinates will remove the marker. Anchor: "bottom" - marker will be shown
-above the cursor while setting (centered to the cursos by default).
+// First steps of fetching data "favorite places" from API by "onMounted" hook (analogy to
+"useEffect" in React). Instead of static data we are fetching it from the API and store it in
+"favoritePlaces" variable (which is a referential, dynamic, object now).
