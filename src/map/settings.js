7333ec21724f686a38730d6f6d712d2ffc386de6
Oleh Shalashov <spirit14081992@gmail.com>
2025-07-29 18:18:01 -0300

    Add settings for deploying on Vercel

--- src/map/settings.js
+++ src/map/settings.js
@@ -1,6 +1,9 @@
-import { token } from './token.js'
+const getToken = () => {
+  const token = import.meta.env.VITE_MAPBOX_TOKEN
+  return token
+}
 
 export const mapSettings = {
   style: 'mapbox://styles/mapbox/streets-v12',
-  apiToken: token,
+  apiToken: getToken(),
 }
